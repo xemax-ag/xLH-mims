27395e7887a3d5d886eaae7169c730c19551bc89
xemax-pascal <pascal.helfenstein@xemax.ch>
2025-12-01 11:50:30 +0100

    Update docker compose

--- win_docker_update_log.bat
+++ win_docker_update_log.bat
@@ -1,2 +1,2 @@
-docker compose -f compose_xlh_mims.yaml down --remove-orphans
-docker compose -f compose_xlh_mims.yaml up --pull always
+docker compose -f win_compose_xlh_mims.yaml down --remove-orphans
+docker compose -f win_compose_xlh_mims.yaml up --pull always
