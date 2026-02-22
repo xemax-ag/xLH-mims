2590475b6a1dcb2f0e528f9e7da8225b23111af1
xemax-pascal <pascal.helfenstein@xemax.ch>
2026-02-22 07:59:40 +0100

    Marimo Notebooks

--- xlh_mims_docker_install.sh
+++ xlh_mims_docker_install.sh
@@ -3,6 +3,7 @@
 # curl -L "https://raw.githubusercontent.com/xemax-ag/xLH-mims/refs/heads/main/xlh_mims_docker_install.sh" -o "xlh_mims_docker_install.sh"
 # chmod +x xlh_mims_docker_install.sh
 # bash xlh_mims_docker_install.sh
+rm xlh_mims_docker_install.sh
 curl -L "https://raw.githubusercontent.com/xemax-ag/xLH-mims/refs/heads/main/docker_compose_xlh_mims_win.yaml" -o "docker_compose_xlh_mims_win.yaml"
 docker compose -f docker_compose_xlh_mims_win.yaml down --remove-orphans
 docker compose -f docker_compose_xlh_mims_win.yaml up --pull always -d
